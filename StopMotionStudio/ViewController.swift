d0ece35ed14f60e865fc004cb63b9689fe776557
shuntaro <shuntar30@gmail.com>
2020-06-01 11:35:44 +0900

    Initial Commit

---
+++ StopMotionStudio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StopMotionStudio
+//
+//  Created by Shuntaro Kasatani on 6/1/20.
+//  Copyright © 2020 Shuntaro Kasatani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
